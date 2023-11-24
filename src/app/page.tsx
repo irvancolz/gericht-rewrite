b0eb3c64221ff3a8fd310db93eda412e9bb18e6b
nugroho <nugrohowahidf22@tsgitdev.com>
2023-11-24 20:12:12 +0700

    add basic lenis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
 import { Signature } from "@/components";
+import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis({});
+    function lenisRaf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(lenisRaf);
+    }
+    requestAnimationFrame(lenisRaf);
+  }, []);
+
   return (
     <main className={styles.main}>
       <Signature rotation="right" />
