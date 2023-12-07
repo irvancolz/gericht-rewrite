dcfcd3afa27efe7410675c3e1822c38c3775d250
nugroho <nugrohowahidf22@tsgitdev.com>
2023-12-07 07:40:11 +0700

    add simple animation in chef words section

--- src/components/ui/chef_words/chef_word.tsx
+++ src/components/ui/chef_words/chef_word.tsx
@@ -1,7 +1,9 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useLayoutEffect, useRef } from "react";
 import style from "./chef_word.module.css";
 import { Carousel, SectionTitle } from "@/components";
+import { gsap } from "gsap";
+import Splitting from "splitting";
 
 const CONTENT = [
   {
@@ -25,25 +27,11 @@ const CONTENT = [
   },
 ];
 
-const CHEF_IMAGES = [
-  "/assets/png/chef_word_1.png",
-  "/assets/png/chef_word_2.png",
-];
-
-const CHEF_QUOTES = [
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit
-  . auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget
-  sit. Nulla scelerisque scelerisque congue ac consequat, aliquam
-  molestie lectus eu. Congue iaculis integer curabitur semper sit
-  nunc.`,
-  `Duis a sapien at leo ullamcorper viverra. Mauris lobortis mattis orci,
-  a volutpat justo interdum eu. Aenean vel interdum arcu. Suspendisse
-  aliquet eget nisi eget pharetra. Vivamus blandit est quam, ultricies 
-  cursus ex vehicula non.`,
-];
-
 export function ChefWord() {
   const [activeSlide, setActiveSlide] = useState(1);
+  const [quotes, setQuotes] = useState<any[]>([]);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const quotesRef = useRef<HTMLParagraphElement | null>(null);
 
   function updateSlide(i: number) {
     if (i > CONTENT.length - 1) {
@@ -52,6 +40,36 @@ export function ChefWord() {
     }
     setActiveSlide(i);
   }
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.from(containerRef.current, {
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+          // markers: true,
+          toggleActions: "play none nore reverse",
+          start: "top 40%",
+          trigger: containerRef.current,
+        },
+      });
+    }, containerRef);
+
+    return () => ctx.revert();
+  }, []);
+
+  useLayoutEffect(() => {
+    const animaton = gsap.context(() => {
+      gsap.from(".chef_words_content", {
+        opacity: 0,
+        y: 30,
+        duration: 1,
+        stagger: 0.1,
+      });
+    }, containerRef);
+
+    return () => animaton.revert();
+  }, [activeSlide]);
 
   useEffect(() => {
     const time = setInterval(() => {
@@ -62,7 +80,7 @@ export function ChefWord() {
   });
 
   return (
-    <div className={style.container}>
+    <div ref={containerRef} className={style.container}>
       <div className={style.content_wrapper}>
         <Carousel
           activeTabs={activeSlide}
@@ -74,14 +92,25 @@ export function ChefWord() {
             desc="What we believe in"
             title={"Chef`s Word"}
           />
-          <p className={style.quotes}>
+          <p
+            className={`${style.quotes} chef_words_content`}
+            ref={quotesRef}
+            data-splitting="true"
+          >
             <i>{CONTENT[activeSlide].quotes}</i>
           </p>
           <p className={style.title}>
-            {CONTENT[activeSlide].name} <br />
-            <span> {CONTENT[activeSlide].title}</span>
+            <span className="chef_words_content">
+              {CONTENT[activeSlide].name}
+            </span>
+            <br />
+            <span className="chef_words_content">
+              {CONTENT[activeSlide].title}
+            </span>
           </p>
-          <p className={style.name}>{CONTENT[activeSlide].name}</p>
+          <p className={`${style.name} chef_words_content`}>
+            {CONTENT[activeSlide].name}
+          </p>
         </div>
       </div>
     </div>
